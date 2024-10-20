70cf2193893d102af718484f0d1ae20b1b561e8d
BartlomiejSokalski <bartlomiej@sokalski.job@gmail.com>
2024-10-20 17:41:10 +0200

    responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0024dce9996f529b85b18ec8e0eb5af0911f88eef04242d25ca517aa16d6cf6
-size 11314
+oid sha256:ce15d73894843e7fcb1d762c6106095ea760da0de31ede12cf9d9950361fd979
+size 11383
