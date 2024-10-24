a6e941c19270a7b4fc08480d4c6eace8ecab4bd0
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-10-24 12:39:01 +0200

    impoemented env

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      VITE_BACKEND_URL: JSON.stringify(env.VITE_BACKEND_URL),
     },
-  },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
