e7410766dee443e15bee5f7565def81f90fd5bb2
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-10-31 15:33:57 +0100

    added Activity page

---
+++ src/pages/Activity.tsx
@@ -0,0 +1,11 @@
+const Activity = () => {
+    return (
+        <div className="bg-[#F5F5F5] min-h-screen py-10">
+            <div className="w-11/12 mx-auto py-8">
+                <h1 className="text-3xl font-bold mb-6 text-center">Dzialanosci</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Activity;
