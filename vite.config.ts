a863fd7263d648a05b2815464037650fbad2fc4a
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-11-03 14:31:24 +0100

    added env veriables

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), "");
     return {
         define: {
+            VITE_EMAILJS_SERVICE_ID: JSON.stringify(env.VITE_EMAILJS_SERVICE_ID),
+            VITE_EMAILJS_TAMPLATE_ID: JSON.stringify(env.VITE_EMAILJS_TAMPLATE_ID),
+            VITE_EMAILJS_PB_KEY: JSON.stringify(env.VITE_EMAILJS_PB_KEY),
+            VITE_EMAILJS_SK_KEY: JSON.stringify(env.VITE_EMAILJS_SK_KEY),
             VITE_BACKEND_URL: JSON.stringify(env.VITE_BACKEND_URL),
             VITE_AWS_BUCKET_NAME: JSON.stringify(env.VITE_AWS_BUCKET_NAME),
             VITE_AWS_REGION: JSON.stringify(env.VITE_AWS_REGION),
